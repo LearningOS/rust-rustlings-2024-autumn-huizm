f05b361b24e6ba20cf94bf4ecbda5d4de8ae7316
Zeming Hui <me@huizm.cn>
2024-10-17 15:14:37 +0800

    Complete algorithm2

--- exercises/algorithm/algorithm2.rs
+++ exercises/algorithm/algorithm2.rs
@@ -2,7 +2,6 @@
 	double linked list reverse
 	This problem requires you to reverse a doubly linked list
 */
-// I AM NOT DONE
 
 use std::fmt::{self, Display, Formatter};
 use std::ptr::NonNull;
@@ -74,6 +73,13 @@ impl<T> LinkedList<T> {
     }
 	pub fn reverse(&mut self){
 		// TODO
+        let mut curr = self.start;
+        while let Some(curr_node) = curr {
+            std::mem::swap(unsafe { &mut (*curr_node.as_ptr()).prev }, unsafe { &mut (*curr_node.as_ptr()).next });
+            curr = unsafe { (*curr_node.as_ptr()).prev };
+        }
+
+        std::mem::swap(&mut self.start, &mut self.end);
 	}
 }
 
